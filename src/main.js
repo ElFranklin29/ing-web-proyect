e427fdddc84162f9b9b48258d1f4241b635f2013
Franklin <franklin2908david@gmail.com>
2025-05-26 23:02:50 -0500

    AGREGAR codigo para conectar con GTM

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import router from './router'
 
+app.use(VueGtm, {
+  id: 'GTM-59G6W7LB', // tu GTM ID
+  enabled: true,
+  debug: true,
+  vueRouter: router, // si usas vue-router
+});
+
 
 createApp(App).use(router).mount('#app')
 
