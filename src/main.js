711b63f311d4d1119b9e0e34bc8c121d0c46b36c
Franklin <franklin2908david@gmail.com>
2025-05-27 16:15:25 -0500

    AGREGAR codigo para eliminar CSS no usado

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import VueGtmPlugin from 'vue-gtm/dist/plugin'
+
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import purgecss from '@fullhuman/postcss-purgecss'
 
-// https://vite.dev/config/
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+     
+        ...(isProduction
+          ? [
+              purgecss({
+                content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+                defaultExtractor: content =>
+                  content.match(/[\w-/:]+(?<!:)/g) || [],
+              })
+            ]
+          : [])
+      ]
+    }
+  }
 })
